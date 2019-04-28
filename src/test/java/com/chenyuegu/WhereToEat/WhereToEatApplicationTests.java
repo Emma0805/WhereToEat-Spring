1a41f4994ba04153bbaedc5025307681445d2d86
Emma Gu <emma@Emmas-MacBook-Pro.local>
2019-04-28 09:34:54 -0500

    init code

---
+++ src/test/java/com/chenyuegu/WhereToEat/WhereToEatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chenyuegu.WhereToEat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhereToEatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
